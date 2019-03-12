d57fce8c289af3b7ab19dc2712575f746dc2dc43
spectator <spectator@s440.bukovszki.intra.net>
2019-03-12 23:18:58 +0100

    bughunt

--- src/net-setup.py
+++ src/net-setup.py
@@ -6,8 +6,10 @@ with open('config/net-config.json') as config_file:
     cfg_data = json.load(config_file)
 
 ## first you must open a connection to the server
-try:
+
 	l = ldap.initialize(cfg_data["ldap"]["ldap_uri"])
+
+try:
 	l.protocol_version = ldap.VERSION3	
 except ldap.LDAPError, e:
 	print e
@@ -20,18 +22,8 @@ retrieveAttributes = None
 
 
 try:
-	ldap_result_id = l.search(baseDN, searchScope, searchFilter, retrieveAttributes)
-	result_set = []
-	while 1:
-		result_type, result_data = l.result(ldap_result_id, 0)
-		if (result_data == []):
-			break
-		else:
-			## here you don't have to append to a list
-			## you could do whatever you want with the individual entry
-			## The appending to list is just for illustration. 
-			if result_type == ldap.RES_SEARCH_ENTRY:
-				result_set.append(result_data)
-	print result_set
+	result = l.search_s(baseDN, searchScope, searchFilter, retrieveAttributes)
+	print result
+    l.unbind_s()
 except ldap.LDAPError, e:
 	print e
